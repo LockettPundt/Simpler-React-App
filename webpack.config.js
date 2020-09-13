f56ff1c8fbf5471912b538acfe9ce5775a3a2241
Lockett Pundt <kalifornium@hotmail.com>
2020-09-13 18:08:29 -0400

    build: :tada: initial webpack config
    initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: './',
+    filename: 'index_bundle.js',
+  },
+  devServer: {
+    contentBase: './build',
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve('./public/index.html'),
+    }),
+  ],
+};
