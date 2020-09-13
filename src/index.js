451b2daaf32bf705849f82e55a2a4c2ac04d5cde
Lockett Pundt <kalifornium@hotmail.com>
2020-09-13 18:09:57 -0400

    feat: :tada: src dit and entry point for app
    src dit and entry point for app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = () => (
+
+  <h1>Simpler React Stater!</h1>
+
+);
+ReactDOM.render(<Index />, document.getElementById('root'));
